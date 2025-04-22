b089890973ac586a1562a68d72338114630db70d
Emerson <emersonnjunior2006@gmail.com>
2025-04-22 14:46:59 -0300

    end

--- src/App.java
+++ src/App.java
@@ -1,9 +1,38 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-        System.out.println("digite o nome de uma estacao: ");
-        Scanner Scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número do mês (1 a 12): ");
+        int mes = scanner.nextInt();
+
+        if (mes < 1 || mes > 12) {
+            System.out.println("Mês inválido.");
+            return;
+        }
+
+        Estacoes estacaoAtual = null;
+
+        for (Estacoes estacao : Estacoes.values()) {
+            if (estacao.contMes(mes)) {
+                estacaoAtual = estacao;
+                break;
+            }
+        }
+
+        if (estacaoAtual != null) {
+            System.out.println("\nEstação do ano: " + estacaoAtual.name());
+            System.out.println("Meses dessa estação:");
+
+            for (int m : estacaoAtual.getMeses()) {
+                if (m == mes) {
+                    System.out.println("**" + m + "**");
+                } else {
+                    System.out.println(m);
+                }
+            }
+        }
+
     }
 }
